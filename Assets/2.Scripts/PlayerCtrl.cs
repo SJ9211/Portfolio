a093377ac4f759269f57e9393373defafe4e66aa
SJ9211 <jsj921127@naver.com>
2025-04-10 12:41:15 +0900

    new

---
+++ Assets/2.Scripts/PlayerCtrl.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerCtrl : MonoBehaviour
+{ 
+    public float speed = 10.0f;
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        
+    }
+}

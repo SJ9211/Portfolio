17deee46f6fb7f5f8ceaa226ff6f06994c790274
SJ9211 <jsj921127@naver.com>
2025-04-17 19:44:35 +0900

    NEW3

--- Assets/2.Scripts/BulletCtrl.cs
+++ Assets/2.Scripts/BulletCtrl.cs
@@ -1,36 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography;
 using UnityEngine;
 
 public class BulletCtrl : MonoBehaviour
 {
     public float damage = 20.0f;
-    public float force = 3000.0f;
+    public float force = 5000.0f;
 
     private Rigidbody rb;
-    private Transform tr;
-    
-    
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
 
         rb.AddForce(transform.forward * force);
     }
-
-
-     void OnTriggerEnter(Collider other)
-    {
-        // 충돌한 오브젝트가 Zombie 일 경우 해당 오브젝트에 데미지를 줌
-        if ( other.gameObject.tag == "ZOMBIE")
-        {
-            Debug.Log("ZOMBIE");
-
-            // 몬스터 피해
-            other.GetComponent<MonsterCtrl>();
-        }
-        // 충돌 총알 제거
-        Destroy(tr.gameObject);
-    }
 }
+  
--- Assets/2.Scripts/RemoveBullet.cs
+++ Assets/2.Scripts/RemoveBullet.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class RemoveBullet : MonoBehaviour
 {
+    public GameObject sparkEffect;
     void OnCollisionEnter(Collision coll)
     {
         // 충돌한 게임오브젝트 태그값
